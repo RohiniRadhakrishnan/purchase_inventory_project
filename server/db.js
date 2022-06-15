9c80f136c9011b001fa81bd5b761522dabeb858f
Nagarohini R <naga3009@global.chainsys.com>
2022-06-15 15:34:52 +0530

    rohs

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 const Cloudant = require('@cloudant/cloudant');
-let url =
+const url =
   'https://75b0afe3-3fa7-477b-8352-bdcfcd522a16-bluemix.cloudantnosqldb.appdomain.cloud/';
-let username = 'apikey-v2-2djdlrrbf736ap4aa6rlre2x1j1wf65v1ti1e8x2bihn';
-let password = '3bc2893c0a2a1ec42d9b17840b18447b';
-let cloudant = Cloudant({ url: url, username: username, password: password });
+const username = 'apikey-v2-2djdlrrbf736ap4aa6rlre2x1j1wf65v1ti1e8x2bihn';
+const password = '3bc2893c0a2a1ec42d9b17840b18447b';
+const cloudant = Cloudant({ url: url, username: username, password: password });
 
 let insert = function (paramsvalue) {
   console.log(paramsvalue);
